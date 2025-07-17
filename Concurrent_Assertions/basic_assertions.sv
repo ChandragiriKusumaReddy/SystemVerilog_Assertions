b97e8bf04832f6bf739b662ab2befd81adff72b4
Chandragiri Kusuma Reddy <kusumachandragiri17@gmail.com>
2025-07-17 15:49:05 +0530

    Update basic_assertions.sv

--- Concurrent_Assertions/basic_assertions.sv
+++ Concurrent_Assertions/basic_assertions.sv
@@ -1,11 +1,27 @@
 module basic_assertions(input logic clk, input logic rst, input logic a, b);
 
-  // Ensure that a and b are never high at the same time
   property p_exclusive;
     !(a && b);
   endproperty
 
   assert property (@(posedge clk) disable iff (rst) p_exclusive)
-    else $error("a and b should not be high at the same time");
+    else $error("Assertion Failed: a and b both high!");
 
 endmodule
+
+module tb;
+  logic clk = 0, rst = 0, a = 0, b = 0;
+  always #5 clk = ~clk;
+
+  basic_assertions dut(clk, rst, a, b);
+
+  initial begin
+    #12 a = 1; b = 1; // Violation
+    #10 a = 0; b = 1;
+    #10 $finish;
+  end
+endmodule
+
+
+//Output:
+    # KERNEL: Error: testbench.sv (9): Assertion Failed: a and b both high!
