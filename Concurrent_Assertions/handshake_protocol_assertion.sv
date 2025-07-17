e9fe25bcc61df8ce8955193cad1d48ca689aadca
Chandragiri Kusuma Reddy <kusumachandragiri17@gmail.com>
2025-07-17 15:53:23 +0530

    Update handshake_protocol_assertion.sv

--- Concurrent_Assertions/handshake_protocol_assertion.sv
+++ Concurrent_Assertions/handshake_protocol_assertion.sv
@@ -1,6 +1,5 @@
 module handshake_protocol_assertion(input logic clk, input logic req, input logic ack);
 
-  // Once req is high, ack should come within 3 cycles
   property p_handshake;
     @(posedge clk) req |=> ##[1:3] ack;
   endproperty
@@ -8,3 +7,22 @@ module handshake_protocol_assertion(input logic clk, input logic req, input logi
   assert property(p_handshake);
 
 endmodule
+
+module tb;
+  logic clk = 0, req = 0, ack = 0;
+  always #5 clk = ~clk;
+
+  handshake_protocol_assertion dut(clk, req, ack);
+
+  initial begin
+    #10 req = 1;
+    #10 req = 0;
+    #20 ack = 1;  // Delayed beyond 3 cycles → FAIL
+    #10 $finish;
+  end
+endmodule
+
+
+
+//Output:
+    # ** Error: handshake_protocol_assertion.sv(6): Assertion p_handshake failed
